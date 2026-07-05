abd731f7752e65dc73fd65c7db34a0ed7a0be3a4
PayalAnkusharaoFullstack <ankusharaopayal@gmail.com>
2026-07-05 21:09:45 +0530

    Added Pattern09 to Pattern22

---
+++ Patterns/Pattern15.java
@@ -0,0 +1,14 @@
+public class Pattern15 {
+    
+              public static void main(String args[]){
+        int n=3;
+        for(int i=n;i>=1;i--){
+            for(char j='A';j<='A'+i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+
+    } 
+}
