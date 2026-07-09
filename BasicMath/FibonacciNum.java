183cd56edab27a8a5f3366b76f192aa1415621b2
PayalAnkusharaoFullstack <ankusharaopayal@gmail.com>
2026-07-09 15:40:54 +0530

    Added Basic Math program

---
+++ BasicMath/FibonacciNum.java
@@ -0,0 +1,18 @@
+public class FibonacciNum {
+    public static void main(String[] args) {
+        int n=5;
+        int a=0;
+        int b=1;
+        int c;
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print(a+" ");
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+
+    }
+}
