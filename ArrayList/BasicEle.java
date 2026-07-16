8d2e2b0ab300f9ce8d43814fafe4bb8c093b0c02
PayalAnkusharaoFullstack <ankusharaopayal@gmail.com>
2026-07-16 18:02:46 +0530

    Added ArrayList programs

---
+++ ArrayList/BasicEle.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class BasicEle{
+    public static void main(String[]args){
+
+ArrayList<Integer>cars=new ArrayList<Integer>();
+
+int sum=0;
+int max=Integer.MIN_VALUE;
+int min=Integer.MAX_VALUE;
+
+cars.add(10);
+cars.add(30);
+cars.add(20);
+cars.add(40);
+cars.add(65);
+
+System.out.println(cars);
+
+Collections.sort(cars);
+System.out.println(cars);
+
+System.out.println(cars.size());
+
+for(Integer i:cars){
+    sum=sum+i;
+}
+    System.out.println(sum);
+
+
+for(Integer i:cars){
+if(i>max){
+    max=i;
+}
+}
+   
+System.out.println(max);
+
+
+for(Integer i:cars){
+if(i<min){
+    min=i;
+}
+}
+   
+System.out.println(min);
+
+
+
+
+    }
+
+
+}
