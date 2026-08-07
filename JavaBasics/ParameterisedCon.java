6817567abc25cc6d77319fd4f1ae6aa7936994bd
PayalAnkusharao <ankusharaopayal@gmail.com>
2026-08-07 20:30:07 +0530

    Update ParameterisedCon.java

--- JavaBasics/ParameterisedCon.java
+++ JavaBasics/ParameterisedCon.java
@@ -9,7 +9,7 @@ car(String name,int price){
 }
 public class ParameterisedCon {
     public static void main(String[] args) {
-        car c1=new car("a",6);
+        car c1=new car("a",7);
         System.out.println(c1.name);
         System.out.println(c1.price);
 
